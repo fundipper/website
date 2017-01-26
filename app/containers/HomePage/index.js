9d00da99bd10626f0e320cfefe094994467a8d5f
fundipper <reppidnuf@gmail.com>
2017-01-26 16:11:55 -0500

    material ui app bar

--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -11,14 +11,47 @@
 
 import React from 'react';
 import { FormattedMessage } from 'react-intl';
+import AppBar from 'material-ui/AppBar';
+import {
+  lightGreen500, lightGreen700,
+  pinkA200,
+  grey100, grey300, grey400, grey500,
+  white, darkBlack, fullBlack,
+} from 'material-ui/styles/colors';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import messages from './messages';
+
+const muiTheme = getMuiTheme({
+  palette: {
+    primary1Color: lightGreen500,
+    primary2Color: lightGreen700,
+    primary3Color: grey400,
+    accent1Color: pinkA200,
+    accent2Color: grey100,
+    accent3Color: grey500,
+    textColor: darkBlack,
+    alternateTextColor: white,
+    canvasColor: white,
+    borderColor: grey300,
+    disabledColor: darkBlack,
+    pickerHeaderColor: lightGreen500,
+    clockCircleColor: darkBlack,
+    shadowColor: fullBlack,
+  },
+  appBar: {
+    height: 50,
+  },
+});
 
 export default class HomePage extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
   render() {
     return (
-      <h1>
-        <FormattedMessage {...messages.header} />
-      </h1>
+      <MuiThemeProvider muiTheme={muiTheme}>
+        <AppBar
+          title="Company Name"
+        />
+      </MuiThemeProvider>
     );
   }
 }
